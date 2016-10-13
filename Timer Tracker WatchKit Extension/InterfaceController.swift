bd63226f79c3be230e636b9f9ec8ae1445efc3c7
Daniel Rocha <daniel.rocha@piggypeg.com>
2016-10-13 15:22:20 -0300

    clena up code

--- Timer Tracker WatchKit Extension/InterfaceController.swift
+++ Timer Tracker WatchKit Extension/InterfaceController.swift
@@ -23,22 +23,7 @@ class InterfaceController: WKInterfaceController {
     
         // Configure interface objects here.
         updateUI(clockedIn: clockedIn)
-    }
     
-    func updateUI(clockedIn: Bool) {
-        if clockedIn {
-            //THE UI FOR WHEN SOMEONE IS CLOCKED IN
-            topLabel.setHidden(false)
-            middleLabel.setText("Today\n5m 22s")
-            button.setTitle("Clocked-Out")
-            button.setBackgroundColor(UIColor.red)
-        } else {
-            //THE UI FOR WHEN SOMEONE IS CLOCKED OUT
-            topLabel.setHidden(true)
-            middleLabel.setText("Today\n3h 44m")
-            button.setTitle("Clocked-In")
-            button.setBackgroundColor(UIColor.green)
-        }
     }
     
     override func willActivate() {
@@ -49,6 +34,25 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func updateUI(clockedIn: Bool) {
+        if clockedIn {
+            // THE UI FOR WHEN SOMEONE IS CLOCKED IN
+            
+            topLabel.setHidden(false)
+            self.topLabel.setText("Today: \(self.totalTimeWorkedAsString())")
+            middleLabel.setText("0s")
+            button.setTitle("Clocked-Out")
+            button.setBackgroundColor(UIColor.red)
+        } else {
+            // THE UI FOR WHEN SOMEONE IS CLOCKED OUT
+            
+            topLabel.setHidden(true)
+            middleLabel.setText("Today\n\(self.totalTimeWorkedAsString())")
+            button.setTitle("Clocked-In")
+            button.setBackgroundColor(UIColor.green)
+        }
     }
 
     @IBAction func clockInOutTapped() {
@@ -68,6 +72,7 @@ class InterfaceController: WKInterfaceController {
         
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(timer) in
             if let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date {
+                
                 let timeInterval = Int(Date().timeIntervalSince(clockedInDate))
                 print(timeInterval)
                 
@@ -75,7 +80,20 @@ class InterfaceController: WKInterfaceController {
                 let minutes = (timeInterval % 3600) / 60
                 let seconds = timeInterval % 60
                 
-                self.middleLabel.setText("\(hours)h \(minutes)m \(seconds)s")   
+                var currentClockedInString = ""
+                
+                if hours != 0 {
+                    currentClockedInString += "\(hours)h"
+                }
+                if minutes != 0 || hours != 0 {
+                    currentClockedInString += " \(minutes)m"
+                }
+                
+                currentClockedInString += " \(seconds)s"
+                
+                self.middleLabel.setText(currentClockedInString)
+            
+                self.topLabel.setText("Today: \(self.totalTimeWorkedAsString())")
             }
         })
     }
@@ -87,7 +105,7 @@ class InterfaceController: WKInterfaceController {
             if var clockIns = UserDefaults.standard.array(forKey: "clockIns") as? [Date] {
                 clockIns.insert(clockedInDate, at: 0)
                 UserDefaults.standard.set(clockIns, forKey: "clockIns")
-                print(clockIns)
+                //print(clockIns)
             } else {
                 //FIRST TIME
                 UserDefaults.standard.set([clockedInDate], forKey: "clockIns")
@@ -97,7 +115,7 @@ class InterfaceController: WKInterfaceController {
             if var clockOuts = UserDefaults.standard.array(forKey: "clockOuts") as? [Date] {
                 clockOuts.insert(Date(), at: 0)
                 UserDefaults.standard.set(clockOuts, forKey: "clockOuts")
-                print(clockOuts)
+                //print(clockOuts)
             } else {
                 //FIRST TIME
                 UserDefaults.standard.set([Date()], forKey: "clockOuts")
@@ -107,5 +125,48 @@ class InterfaceController: WKInterfaceController {
         }
         
         UserDefaults.standard.synchronize()
+        
+        totalClockedTime()
+    }
+    
+    func totalClockedTime() -> Int {
+        if var clockIns = UserDefaults.standard.array(forKey: "clockIns") as? [Date] {
+            if var clockOuts = UserDefaults.standard.array(forKey: "clockOuts") as? [Date] {
+                
+                var seconds = 0
+                
+                for index in 0..<clockIns.count {
+                    
+                    // Find the seconds between clockin and out
+                    let currentSeconds = Int(clockOuts[index].timeIntervalSince(clockIns[index]))
+                
+                    // Add time to seconds
+                    seconds += currentSeconds
+                }
+                print("Total seconds: \(seconds)")
+                return seconds
+            }
+        }
+        
+        return 0
+    }
+    
+    func totalTimeWorkedAsString() -> String {
+        
+        var currentClockedInSeconds = 0
+        
+        if let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date {
+            
+            currentClockedInSeconds = Int(Date().timeIntervalSince(clockedInDate))
+            
+        }
+        
+            let totalTimeInterval = currentClockedInSeconds + self.totalClockedTime()
+            
+            let totalHours = totalTimeInterval / 3600
+            let totalMinutes = (totalTimeInterval % 3600) / 60
+            let totalSeconds = totalTimeInterval % 60
+        
+            return "\(totalHours)h \(totalMinutes)m"
     }
 }
